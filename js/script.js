0695518713757d9ca17b2fe7bef295decce384d1
Mikołaj Graczyk <mikolajgraczyk123@gmail.com>
2022-11-14 20:06:50 +0100

    fixed result display issue

--- js/script.js
+++ js/script.js
@@ -15,21 +15,23 @@ submitElement.addEventListener("click", (event) => {
     let currency;
 
     switch (rate) {
-        case "4.68":
+        case "4.55":
             currency = "USD";
             break;
-        case "4.70":
+        case "4.71":
             currency ="EUR";
             break;
-        case "4.76":
+        case "4.83":
             currency = "CHF";
             break;
-        case "3.35":
+        case "3.32":
             currency = "SGD";
             break;
         default:
             currency = "";
     }
+
+    console.log(currency);
 
     if(inputElement.value === "") {
         return;
